b72aa358062b6af1a1f67336e58740550ab73096
radwanromy <radwanromy@gmail.com>
2021-11-02 15:10:24 +0600

    first commit

---
+++ src/main/java/net/javaguides/sms/service/StudentService.java
@@ -0,0 +1,17 @@
+package net.javaguides.sms.service;
+
+import java.util.List;
+
+import net.javaguides.sms.entity.Student;
+
+public interface StudentService {
+	List<Student> getAllStudents();
+	
+	Student saveStudent(Student student);
+	
+	Student getStudentById(int id);
+	
+	Student updateStudent(Student student);
+	
+	void deleteStudentById(int id);
+}
